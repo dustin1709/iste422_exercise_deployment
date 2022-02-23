4b166bd8371f936081cbecfa3d22dc6882dc11d7
Kevin Liu <kl2259@g.rit.edu>
2022-02-23 18:11:35 -0500

    reverted implementation + added logging for app startup

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,5 +1,12 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RunEdgeConvert {
+   
+   public static Logger logger = LogManager.getLogger(RunEdgeConvert.class.getName());
+
    public static void main(String[] args) {
       EdgeConvertGUI edge = new EdgeConvertGUI();
+      logger.info(String.format("Starting Edge Converter"));
    }
 }
