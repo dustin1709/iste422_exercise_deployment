8d9ba4903e81f940300bd7b37d53344d1986413e
Kevin Liu <kl2259@g.rit.edu>
2022-02-24 08:48:04 -0500

    logging added for EdgeConnector, EdgeConnectorTest + RunEdgeConnect changed to debug level log

--- src/test/java/EdgeConnectorTest.java
+++ src/test/java/EdgeConnectorTest.java
@@ -1,13 +1,19 @@
 import static org.junit.Assert.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class EdgeConnectorTest {
+
+	public static Logger logger = LogManager.getLogger(EdgeConnectorTest.class.getName());
+
 	EdgeConnector testObj;
 
 	@Before
 	public void setUp() throws Exception {
+		logger.debug(String.format("Setting up Test Edge Connector."));
 		testObj = new EdgeConnector("1|2|3|testStyle1|testStyle2");
 	}
 
@@ -17,6 +23,7 @@ public class EdgeConnectorTest {
 		String opt1Str = System.getProperty("optionone");
 		final long opt1;
 		if (opt1Str == null) {
+			logger.warn(String.format("No Number Connector Value"));
 			opt1 = 1;
 		}
 		else {
@@ -67,24 +74,32 @@ public class EdgeConnectorTest {
 
 	@Test
 	public void testSetIsEP1Field() {
+		logger.info(String.format("Setting EP1 Table: false"));
+      	logger.debug(String.format("EP1 Table is now: false"));
 		testObj.setIsEP1Field(false);
 		assertEquals("isEP1Field should be what you set it to",false,testObj.getIsEP1Field());
 	}
 
 	@Test
 	public void testSetIsEP2Field() {
+		logger.info(String.format("Setting EP2 Field: false"));
+      	logger.debug(String.format("EP2 Field is now: false"));
 		testObj.setIsEP2Field(false);
 		assertEquals("isEP2Field should be what you set it to",false,testObj.getIsEP2Field());
 	}
 
 	@Test
 	public void testSetIsEP1Table() {
+		logger.info(String.format("Setting EP1 Table: false"));
+      	logger.debug(String.format("EP1 Table is now: false"));
 		testObj.setIsEP1Table(false);
 		assertEquals("isEp1Table should be what you set it to",false,testObj.getIsEP1Table());
 	}
 
 	@Test
 	public void testSetIsEP2Table() {
+		logger.info(String.format("Setting EP2 Table: false"));
+      	logger.debug(String.format("EP2 Table is now: false"));
 		testObj.setIsEP2Table(false);
 		assertEquals("isEp2Table should be what you set it to",false,testObj.getIsEP2Table());
 	}
