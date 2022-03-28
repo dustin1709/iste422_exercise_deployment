225216c5eaf373d708724097b5384727b0a55069
Regine <ginou10@msn.com>
2022-03-27 22:41:17 -0400

    EdgeConvertDDL tests

---
+++ src/test/java/ConcreteDDL.java
@@ -0,0 +1,33 @@
+public class ConcreteDDL extends EdgeConvertCreateDDL {
+
+// 
+
+    public ConcreteDDL(EdgeTable[] tables, EdgeField[] fields){
+        super(tables, fields);    
+    }
+
+    @Override
+    public String getDatabaseName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getProductName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getSQLString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void createDDL() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
